05631fa28321020a769b68b038764a43279fa651
Daniel <42457630+gusevdaniel@users.noreply.github.com>
2022-01-21 12:39:43 +0700

    Fixed bugs and improved performance

--- visualization/main.py
+++ visualization/main.py
@@ -4,225 +4,140 @@ import pandas as pd
 from bokeh.io import curdoc
 from bokeh.plotting import figure
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, TableColumn, DataTable
+from bokeh.models import ColumnDataSource
 from bokeh.models import HoverTool, WheelZoomTool
-from bokeh.models import LabelSet, Select, Slider, MultiChoice
+from bokeh.models import Select
 from bokeh.models.callbacks import CustomJS
 
-from sklearn.neighbors import KDTree
 
-COLUMNS = ['x', 'y', 'ent', 'lang', 'ent_tr', 'type', 'color', 'sizes', 'distance']
-
-tooltips = [('Entity', '@ent' + ' (@lang)'), ('Type', '@type')]
-
-def prepare_df():
-    df = pd.read_csv(filepath)
-
-    df['color'] = np.where(df['lang'] == 'en', "blue", "red")
-    df['sizes'] = 5
-
-    df['distance'] = np.nan
-
-    return df
-
-def df_to_cds(df):
-    return ColumnDataSource(data=df)
-
-def get_index(df, ent):
-    ent_index = df.loc[df['ent'].isin([ent])].index[0]
-
-    return ent_index
-
-def set_color(df, ent_index):
-    ent_lang = df.loc[ent_index]['lang']
-
-    if ent_lang == 'en':
-        df.at[ent_index, 'color'] = 'blue'
-    else:
-        df.at[ent_index, 'color'] = 'red'
-
-    df.at[ent_index, 'sizes'] = 10
-
-def df_update(df, ent):
-    df['color'] = 'lightgray'
-    df['sizes'] = 5
-
-    if ent != 'None':
-        ent_index = get_index(df, ent)
-        set_color(df, ent_index)
-
-        ent_tr = df.loc[ent_index]['ent_tr']
-        try:
-            ent_index = get_index(df, ent_tr)
-            set_color(df, ent_index)
-        except IndexError as error:
-            pass
-    else:
-        df['color'] = np.where(df['lang'] == 'en', "blue", "red")
-
-    return df_to_cds(df)
-
-def find_neighbours(df, ent, ent_number):
-    ent_id = get_index(df, ent)
-
-    new_df = pd.DataFrame()
-    new_df['x'] = df['x']
-    new_df['y'] = df['y']
-    ent_embeds = new_df.to_numpy()
-
-    # save original indexes
-    i = 0
-    df_ids = {}
-    np_ids = {}
-    df_indexes = list(df.index)
-    for elem in df_indexes:
-        df_ids[elem] = i
-        np_ids[i] = elem
-        i += 1
-
-    new_ent_id = df_ids[ent_id]
-
-    kdt = KDTree(ent_embeds)
-    kdt_distance, kdt_indexes = kdt.query([ent_embeds[new_ent_id]], k=ent_number)
-
-    new_kdt_indexes = []
-    for ind in kdt_indexes[0]:
-        new_kdt_indexes.append(np_ids[ind])
-
-    selected_df = df.loc[new_kdt_indexes]
-
-    kdt_distance = kdt_distance.tolist()[0]
-    selected_df['distance'] = kdt_distance
-
-    return selected_df
-
-def update_types():
-    global df
-
-    current_types = types_choice.value
-
-    if 'All' in current_types or current_types == []:
-        df = prepare_df()
-        src = df_to_cds(df)
-        source.data.update(src.data)
-    else:
-        df = prepare_df()
-        df = df.loc[df['type'].isin(current_types)]
-        src = df_to_cds(df)
-        source.data.update(src.data)
-
-    ents = list(df['ent'])
-    ents.insert(0, 'None')
-
-    ent_select.options = ents 
-    ent_select.value = ents[0]
-    source.selected.indices = []
-
-def update():
-    # entity
-    ent = ent_select.value
-    src = df_update(df, ent)
-    source.data.update(src.data)
-
-    # neighbours
-    if ent != 'None':
-        ent_number = ent_num_slider.value
-        df2 = find_neighbours(df, ent, ent_number)
-
-        src2 = ColumnDataSource(data=df2)
-        source2.data.update(src2.data)
-    else:
-        df2 = pd.DataFrame(columns=COLUMNS)
-
-        src2 = ColumnDataSource(data=df2)
-        source2.data.update(src2.data)
-
-# load data
+# Load data
 folder = 'data\\'
-# filename = 'MultiKE_Word2Vec_EN_RU.csv'
-filename = 'MultiKE_Word2Vec_EN_RU_matched.csv'
+filename = 'MultiKE_Word2Vec_EN_RU.csv'
 filepath = folder + filename
+df_main = pd.read_csv(filepath)
 
-df = prepare_df()
+# Create Column Data Source that will be used by the plot
+source = ColumnDataSource(data=dict(x=[], y=[], ent1_id=[], ent2_id=[], ent1=[], ent2=[], lang=[], type_=[], color=[], size=[], distance=[]))
 
-source = df_to_cds(df)
-source2 = df_to_cds(pd.DataFrame(columns=COLUMNS))
 
-# add interactive elements
-ents = list(df['ent'])
-ents.insert(0, 'None')
-
-ent_select = Select(name='ent_select', title='Сущность', options=(ents), value=ents[0])
-ent_num_slider = Slider(start=10, end=100, value=50, step=1, title='Количество')
-
-types = list(df['type'].unique())
+# Form buttons
+types = list(df_main['type'].unique())
+types = sorted(types)
 types.insert(0, 'All')
-types_choice = MultiChoice(title='Типы', options=types, value=[types[0]])
+select_type = Select(name='select_type', title='Тип', options=types, value=types[0])
 
-# tap event
-callback = CustomJS(args=dict(source=source), code="""
-    const selector = document.getElementsByName('ent_select')[0];
-    const ents = source.data.ent;
+ids = list(df_main['ent1_id'])
+ids = list(map(str, ids))
+ids.insert(0, '-1')
+select_id = Select(name='select_id', title='Номер', options=ids, value=ids[0], visible=False)
 
-    const indices = source.selected.indices;
-    if (indices.length !== 0) {
-        const index = indices[0];
-        const ent = ents[index];
-        selector.value = ent;
-    } else {
-        selector.value = 'None';
-    }
 
-    var event = new Event('change');
-    const cancelled = !selector.dispatchEvent(event);
-""")
-
-# create p1
+# Form screens
 select_tools = ['pan', 'wheel_zoom', 'tap', 'reset', 'save']
+tooltips = [('Entity', '@ent1' + ' (@lang)'), ('Type', '@type_')]
 
 p1 = figure(plot_height=720, plot_width=1280, tools=select_tools, title='Векторное пространство')
 p1.toolbar.active_scroll = p1.select_one(WheelZoomTool)
 p1.add_tools(HoverTool(tooltips=tooltips))
+
+callback = CustomJS(args=dict(source=source), code="""
+    const selector = document.getElementsByName('select_id')[0];
+    const indices = source.selected.indices;
+    if (indices.length !== 0) {
+        selector.value = indices[0];
+    } else {
+        selector.value = -1;
+    }
+    var event = new Event('change');
+    const cancelled = !selector.dispatchEvent(event);
+""")
 p1.js_on_event('tap', callback)
 
 p1.circle(x='x',
-            y='y',
-            source=source,
-            color='color',
-            size='sizes',
-            nonselection_alpha=0.5)
+          y='y',
+          source=source,
+          color='color',
+          size='size',
+          nonselection_alpha=0.5)
 
-# create p2
-p2 = figure(plot_height=720, plot_width=1280, title='Ближайшие сущности')
-p2.add_tools(HoverTool(tooltips=tooltips))
 
-p2.circle(x='x',
-            y='y',
-            source=source2,
-            color='color',
-            size='sizes')
+def set_colors(df):
+    languages = list(df['lang'])
+    colors = list(map(lambda x: 'blue' if x == 'en' else 'red', languages))
+    df['color'] = colors
+    return df
 
-labels = LabelSet(x='x', y='y', text='ent', source=source2)
-p2.add_layout(labels)
 
-# create table
-columns = [ TableColumn(field='ent', title='Сущность'),
-            TableColumn(field='type', title='Тип'),
-            TableColumn(field='distance', title='Расстояние')]
-table = DataTable(source=source2, columns=columns)
+def set_params(df):
+    df = set_colors(df)
+    df['size'] = 5
+    df['distance'] = 0
+    return df
+
+
+def get_color(index):
+    color = 'blue'
+    if index % 2 != 0:
+        color = 'red'
+    return color
+
+
+def emphasize_pair(df, df_ids, id1):
+    row = df.loc[df['ent1_id'] == id1]
+    id2 = row['ent2_id'].values[0]
+    pair = [id1, id2]
+
+    colors = list(map(lambda x: get_color(x) if x in pair else 'lightgray', df_ids))
+    df['color'] = colors
+    sizes = list(map(lambda x: 10 if x in pair else 5, df_ids))
+    df['size'] = sizes
+    return df
+
+
+def get_data():
+    current_type = select_type.value
+    df = df_main.copy()
+    df = set_params(df)
+    if current_type != 'All':
+        df = df.loc[df['type'] == current_type]
+
+    current_id = int(select_id.value)
+    df_ids = list(df['ent1_id'])
+    if current_id != -1:
+        if current_id < len(df_ids):
+            if len(df) < len(df_main):
+                current_id = df_ids[current_id]
+            df = emphasize_pair(df, df_ids, current_id)
+        else:
+            source.selected.indices = []
+
+    return df
+
+
+def update():
+    df = get_data()
+    source.data = dict(
+        x=df['x'], y=df['y'], 
+        ent1_id=df['ent1_id'], ent2_id=df['ent2_id'], 
+        ent1=df['ent1'], ent2=df['ent2'], 
+        lang=df['lang'], type_=df['type'], 
+        color=df['color'], size=df['size'], 
+        distance=df['distance']
+    )
+
 
 # declare controls
-controls = [ent_select, ent_num_slider]
+controls = [select_type, select_id]
 for control in controls:
     control.on_change('value', lambda attr, old, new: update())
 
-controls.insert(0, types_choice)
-types_choice.on_change('value', lambda attr, old, new: update_types())
 
 # set up layout
 inputs = column(*controls, width=320)
-series = column(p1, p2, table)
+series = column(p1)
 fields = column(row(inputs, series), sizing_mode="scale_both")
+
+update()  # initial load of the data
 
 curdoc().add_root(fields)
 curdoc().title = "OpenEA"
